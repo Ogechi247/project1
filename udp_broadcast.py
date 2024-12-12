7ef267670afca3fcc2ea382f7bf6a93164ab171f
Ogechi247 <ogechukwukanmaofili@gmail.com>
2024-12-12 16:18:07 -0500

    Create udp_broadcast.py

---
+++ udp_broadcast.py
@@ -0,0 +1,11 @@
+def udp_broadcast():
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    message = input("Enter a message to broadcast: ").encode()
+    client.sendto(message, ("<broadcast>", 9999))
+    print("Broadcast sent.")
+
+
+# Send a UDP broadcast
+udp_broadcast()
